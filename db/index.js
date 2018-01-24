92b51f0f034bd8161c0d2f2c1518c8f263669304
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-01-24 18:08:18 -0500

    correct ages

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const people = [
-  { id: 1, name: 'Gary', age: 59, city: 'Utica'},
-  { id: 2, name: 'Lisa', age: 58, city: 'Hollywood'},
+  { id: 1, name: 'Gary', age: 60, city: 'Utica'},
+  { id: 2, name: 'Lisa', age: 59, city: 'Hollywood'},
   { id: 3, name: 'Evan', age: 28, city: 'Miami' },
   { id: 4, name: 'Jeremy', age: 25, city: 'New York' },
   { id: 5, name: 'Rachel', age: 22, city: 'Boston' }
